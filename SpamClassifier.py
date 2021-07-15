720f5a228fcefcaf9bb2df5eff1ba44fdfb8c5a1
Haissam Tariq Zaman <haissam.tariq@gmail.com>
2021-07-15 20:31:57 +0500

    added LSTM model

--- SpamClassifier.py
+++ SpamClassifier.py
@@ -2,6 +2,7 @@ import contractions
 import csv
 import nltk
 import re
+from sklearn.model_selection import train_test_split
 
 import numpy as np
 from keras.layers import LSTM
@@ -22,6 +23,10 @@ class SpamClassifier:
     tokenizer = None
     VOC_SIZE = None
     max_length_sequence = None
+    X_train = None
+    X_test = None
+    y_train = None
+    y_test = None
 
     def __init__(self, fileName):
         self.fileName = fileName
@@ -29,6 +34,10 @@ class SpamClassifier:
         self.messages = []
         self.n_label = []
         self.tokenizer = Tokenizer()
+
+    def split_train_test(self):
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.message_sequence, self.n_label,
+                                                                                test_size=0.20, random_state=42)
 
     def read_file(self):
         with open(self.fileName, 'r', encoding='latin-1') as f:
@@ -66,6 +75,8 @@ class SpamClassifier:
                     z += 1
                 self.messages[x][y] = nullSTR
 
+        for x in range(len(self.messages)):
+            self.messages[x] = self.remove_empty_string(self.messages[x])
         # print(self.messages)
 
     def removeURLs(self):
@@ -154,20 +165,20 @@ class SpamClassifier:
         message = message.split()
         message = self.remove_url_msg(message)
         message = self.remove_punc_msg(message)
-        message = self.remove_stop_words_msg(message)
+        message = self.remove_empty_string(message)
+        # message = self.remove_stop_words_msg(message)
         message = " ".join(message)
         message = self.message_to_numeric_msg(message)
         message = self.make_same_size_msg(message)
-        print(message)
-
+        return message
 
     def remove_url_msg(self, message):
         y = 0
         sentence_len = len(message)
         while y < sentence_len:
-            result = re.search("^http://|^https://|^www\.", self.messages[y])
+            result = re.search("^http://|^https://|^www\.", message[y])
             if result is not None:
-                del self.messages[y]
+                del message[y]
                 sentence_len -= 1
             else:
                 y += 1
@@ -198,6 +209,17 @@ class SpamClassifier:
 
         return message
 
+    def remove_empty_string(self, message):
+        msg_len = len(message)
+        x = 0
+        while x < msg_len:
+            if message[x] == "" or message[x] == " ":
+                del message[x]
+                msg_len -= 1
+            else:
+                x += 1
+        return message
+
     def to_lower_case_msg(self, message):
         for y in range(len(message)):
             message[y] = message[y].lower()
@@ -216,40 +238,68 @@ class SpamClassifier:
                 y += 1
         return message
 
-    #TO BE EDITED BY HAISSAM-------------------
+    # TO BE EDITED BY HAISSAM-------------------
     def message_to_numeric_msg(self, message):
-        self.tokenizer.fit_on_texts(message)
-        self.message_sequence = self.tokenizer.texts_to_sequences(message)
-        return message
+        m = []
+        m.append(message)
+        self.tokenizer.fit_on_texts(m)
+        # print(self.tokenizer.word_index)
+        message = self.tokenizer.texts_to_sequences(m)
+        return message[0]
 
     def make_same_size_msg(self, message):
-        for x in message:
-            if len(message) != self.max_length_sequence:
-                diff = self.max_length_sequence - len(message)
-                for i in range(diff):
-                    x.insert(0, 0)
+        if len(message) != self.max_length_sequence:
+            diff = self.max_length_sequence - len(message)
+            for i in range(diff):
+                message.insert(0, 0)
         return message
 
-#----------------------------------------------------------------------------------------------------
+    # ----------------------------------------------------------------------------------------------------
     def LSTMModel(self):
 
-        self.message_sequence = np.array(self.message_sequence)
-        self.n_label = np.array(self.n_label)
+        # self.message_sequence = np.array(self.message_sequence)
+        # self.n_label = np.array(self.n_label)
 
-        print(self.n_label.shape)
+        # print(self.n_label.shape)
+
+        self.split_train_test()
+        self.X_train = np.array(self.X_train)
+        self.y_train = np.array(self.y_train)
 
         model = Sequential()
 
-        feature_num = 100
-        model.add(Embedding(input_dim=self.VOC_SIZE, output_dim=feature_num, input_length=self.max_length_sequence))
-        model.add(LSTM(units=128))
+        feature_num = 50
+        print(self.max_length_sequence)
+        model.add(
+            Embedding(input_dim=self.VOC_SIZE + 20, output_dim=feature_num, input_length=self.max_length_sequence))
+        model.add(LSTM(units=100))
         model.add(Dense(units=1, activation="sigmoid"))
 
         model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
         model.summary()
 
-        model.fit(self.message_sequence, self.n_label, epochs=5, batch_size=32, validation_split=0.2)
+        model.fit(self.X_train, self.y_train, epochs=3, batch_size=32, validation_split=0.2)
 
-        y_pred = model.predict(self.message_sequence)
-        y_pred = (y_pred > 0.5)
-        print(y_pred)
+        # x=self.convertMessage("Txt: CALL to No: 86888 & claim your reward of 3 hours talk time to use from your phone now! Subscribe6GBP/mnth inc 3hrs 16 stop?txtStop www.gamb.tv")
+        # z=self.convertMessage("IMPORTANT - You could be entitled up to £3,160 in compensation from mis-sold PPI on a credit card or loan. Please reply PPI for info or STOP to opt out.")
+        # zz=self.convertMessage("Congratulations! you have won a $1,000 Walmart gift card. Go to http://bit.ly/123456 to claim now.")
+        # xxa=self.convertMessage("Did I forget to tell you ? I want you , I need you, I crave you ... But most of all ... I love you my sweet Arabian steed ... Mmmmmm ... Yummy")
+        # y=self.convertMessage("Hi! I am haissam")
+
+        y_pred = model.predict(self.X_test)
+        for x in range(len(y_pred)):
+            if y_pred[x] < 0.5:
+                y_pred[x] = 0
+            else:
+                y_pred[x] = 1
+
+        from sklearn.metrics import confusion_matrix
+        # Comparing the predictions against the actual observations in y_val
+        cm = confusion_matrix(y_pred, self.y_test)
+
+        def accuracy(confusion_matrix):
+            diagonal_sum = confusion_matrix.trace()
+            sum_of_all_elements = confusion_matrix.sum()
+            return diagonal_sum / sum_of_all_elements
+
+        print("Accuracy is: ", accuracy(cm))
--- main.py
+++ main.py
@@ -7,12 +7,14 @@ def main():
     spam_classifier.tokenize()
     spam_classifier.removeURLs()
     spam_classifier.removePunc()
-    spam_classifier.removeStopWords()
+    #spam_classifier.removeStopWords()
     spam_classifier.labelsToNumeric()
     spam_classifier.join()
     spam_classifier.messageToNumeric()
     spam_classifier.makeSameSize()
     #spam_classifier.printMessageSequence()
     spam_classifier.LSTMModel()
+
+
 if __name__ == '__main__':
     main()
